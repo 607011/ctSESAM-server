d8d67b713a0219c34a5b96feb457c460c1ce2754
Oliver Lau <ola@ct.de>
2015-05-22 15:47:08 +0200

    Added (c) statements.

--- ajax/config.php
+++ ajax/config.php
@@ -1,8 +1,22 @@
 <?php
+/*
 
-//////////////////////////////////
-/// CONFIGURATION OPTIONS
-//////////////////////////////////
+    Copyright (c) 2015 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 $res = array('status' => 'ok');
 
@@ -16,10 +30,14 @@ else if ($_SERVER['SERVER_NAME'] === 'ersatzworld.net' && strpos($_SERVER['REQUE
 // set this option to true to enable persistent database connections; set to false for debugging
 $DB_PERSISTENT = false;
 $DB_NAME = "$DB_PATH/ctpwdgen-server.sqlite";
-$CACERT_NAME = "$DB_PATH/cacert.pem";
 
-if (substr(str_replace("\\", '/', __FILE__), -strlen($_SERVER['PHP_SELF'])) === $_SERVER['PHP_SELF']) {
+function directCall() {
+	return substr(str_replace("\\", '/', __FILE__), -strlen($_SERVER['PHP_SELF'])) === $_SERVER['PHP_SELF'];
+}
+
+if (directCall()) {
     header('Content-Type: text/json');
+    $res['message'] = "Calling " . $_SERVER['PHP_SELF'] . " directly doesn't do anything ;-)";
     echo json_encode($res);
 }
 
--- ajax/globals.php
+++ ajax/globals.php
@@ -1,23 +1,47 @@
 <?php
+/*
+
+    Copyright (c) 2015 Oliver Lau <ola@ct.de>, Heise Medien GmbH & Co. KG
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 require_once 'config.php';
+
+
+$authenticated_user = null;
+
 
 function DEBUG($msg) {
     $timestamp = date('D M j H:i:s.u Y');
     file_put_contents('php://stdout', "[$timestamp] [ctpwdgen:debug] $msg\n");
 }
 
+
 function assert_basic_auth()
 {
+	global $authenticated_user;
 	global $res;
 	if (!isset($_SERVER['PHP_AUTH_USER'])) {
 	    header('WWW-Authenticate: Basic realm="ctpwdgen sync server"');
 	    header('HTTP/1.0 401 Unauthorized');
-	    echo '<p>HTTP basic authentication required</p>';
+	    echo 'HTTP basic authentication required';
 	    exit;
 	}
 	else {
-		$res['user'] = $_SERVER['PHP_AUTH_USER'];
-		$res['pass'] = $_SERVER['PHP_AUTH_PW'];
+		$authenticated_user = $_SERVER['PHP_AUTH_USER'];
 	}
 }
 
