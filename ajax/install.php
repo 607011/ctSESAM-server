916de478af9774eb4a1299f3d784b2c7e3b44371
root <root@FLJSWEB.FLJS22.DE>
2016-01-07 11:41:43 +0100

    Support for MySQL databases

--- ajax/install.php
+++ ajax/install.php
@@ -3,13 +3,13 @@ require_once 'globals.php';
 
 header('Content-type: text/plain');
 if ($dbh) {
-    $dbh->exec('CREATE TABLE IF NOT EXISTS `domains` (' .
-        ' `userid` TEXT PRIMARY KEY,' .
-        ' `data` BLOB' .
+    $dbh->exec('CREATE TABLE IF NOT EXISTS domains (' .
+        ' userid VARCHAR(255) PRIMARY KEY,' .
+        ' data BLOB' .
         ')');
     echo "Table 'domains' created.";
 }
 else {
   echo "Something went wrong.";
 }
-
+?>
